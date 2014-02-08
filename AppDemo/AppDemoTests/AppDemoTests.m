52f20acff51b19f6507c4a4fc02fe5bb3cc627e4
codedemoos <c>
2014-02-08 10:28:27 +0100

    init
    init

---
+++ AppDemo/AppDemoTests/AppDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  AppDemoTests.m
+//  AppDemoTests
+//
+//  Created by Paolo on 08/02/14.
+//  Copyright (c) 2014 Paolo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AppDemoTests : XCTestCase
+
+@end
+
+@implementation AppDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
